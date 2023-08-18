f24ff395c2bbd1be2738677b2bc67bcb95f7809b
Duck Vũ <ngovanvu083@gmail.com>
2023-08-19 01:01:35 +0700

    fix title hero

--- components/home-page/hero.js
+++ components/home-page/hero.js
@@ -7,7 +7,7 @@ const Hero = () => {
       <div className={classes.image}>
         <Image src="/image/site/Me.jpg" alt="Image Me" width={300} height={300} />
       </div>
-      <h1>Hi, I'm Duck</h1>
+      <h1>Hi, I'm TOM</h1>
       <p>I blog about web development - especially front-end framework like React</p>
     </section>
   );
